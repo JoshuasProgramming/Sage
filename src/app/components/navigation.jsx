1814c49d715b36debf9342c72fbe1413e0580c5f
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2024-02-18 12:10:23 +0000

    Added markup skeleton for the navigation. Just have to change background color + add styling

--- src/app/components/navigation.jsx
+++ src/app/components/navigation.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Navigation() {
   return (
-    <div>Navigation</div>
+    <nav>
+        <div className="left">
+            <h1>Brainwave.io</h1>
+        </div>
+
+        <div className="right">
+            <ul>
+                <li>Demos</li>
+                <li>Pages</li>
+                <li>Support</li>
+                <li>Contact</li>
+                <li>Sign In</li>
+                <a href="#">Sign Up</a>
+            </ul>
+        </div>
+    </nav>
   )
 }
 
