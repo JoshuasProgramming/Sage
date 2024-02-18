7467796e3f1c6ae659a2ca87931bec1cb9e51c72
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2024-02-18 14:07:48 +0000

    Finished the the hero section. Will adjust later if needed

---
+++ src/app/components/hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className='text-center items-center pt-73'>
+        {/* // Top of hero section */}
+        <h2 className='text-60 font-bold pb-20'>Pricing & Plans</h2>
+
+        {/* // Bottom of hero section */}
+        <p className='text-base leading-8 font-light'>With lots of unique blocks, you can easily build without coding <br /> Build your next consultancy website within few winutes</p>
+    </section>
+        
+    
+  )
+}
+
+export default Hero;
