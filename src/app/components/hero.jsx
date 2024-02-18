fb7c9d396e1310c5836130b08fee2c7316a6e33b
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2024-02-18 14:19:08 +0000

    adjusted padding on hero h2 and p tags

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Hero() {
   return (
-    <section className='text-center items-center pt-73'>
+    <section className='text-center items-center pt-3'>
         {/* // Top of hero section */}
-        <h2 className='text-60 font-bold pb-20'>Pricing & Plans</h2>
+        <h2 className='text-6xl font-bold pb-12 pt-16'>Pricing & Plans</h2>
 
         {/* // Bottom of hero section */}
-        <p className='text-base leading-8 font-light'>With lots of unique blocks, you can easily build without coding <br /> Build your next consultancy website within few winutes</p>
+        <p className='text-lg leading-8 font-light'>With lots of unique blocks, you can easily build without coding <br /> Build your next consultancy website within few winutes</p>
     </section>
         
     
